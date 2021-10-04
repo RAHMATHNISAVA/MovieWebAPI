b569cded92ea42203b60ac800b53e3eac1ddbde5
RAHMATHNISAVA <ramathnisa28@gmial.com>
2021-10-04 10:09:38 +0100

    Add files via upload

---
+++ MovieWebAPI/Services/MetadataService.cs
@@ -0,0 +1,79 @@
+﻿using MovieWebApplication.Common;
+using MovieWebApplication.Model;
+using MovieWebApplication.Repository.RepositoryInterface;
+using MovieWebApplication.Services.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Services
+{
+    public class MetadataService : IMetadataService
+    {
+       // private readonly IQueryRepository _query;
+        private readonly IMetadataRepository _context;
+
+        public MetadataService(//IQueryRepository query,
+            IMetadataRepository context)
+        {
+            //_query = query;
+            _context = context;
+        }
+
+        //save metadat to given list
+        public bool Post(Metadata metadata)
+        {
+            try
+            {
+                return _context.Insert(metadata);
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //get metadata by given id
+        public Metadata[] Get(int movieId)
+        {
+            try
+            {
+                var allMetadata = _context.GetMetadata();
+                var movies = GetById(allMetadata, movieId);
+                //if (!movies.Any()) throw new Exception("No movies found by that Id");
+                return movies;
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        // Filter the metadata by movieId, grouping by language, selecting the highest id and ordering by language 
+        private static Metadata[] GetById(IEnumerable<Metadata> metadata, int movieId)
+        {
+            try
+            {
+                return metadata
+                    .Where(m => m.MovieId == movieId && m.IsValid())
+                    .GroupBy(m => m.Language, m => m)
+                    .Select(g => g.OrderByDescending(m => m.Id).FirstOrDefault())
+                    .OrderBy(m => m?.Language)
+                    .ToArray();
+            }
+
+             catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
+
---
+++ MovieWebAPI/Services/MovieService.cs
@@ -0,0 +1,77 @@
+﻿using MovieWebApplication.Model;
+using MovieWebApplication.Repository.RepositoryInterface;
+using MovieWebApplication.Services.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMetadataRepository _metadata;
+        private readonly IMoviesRepository _moviedata;
+
+
+        public MovieService(IMetadataRepository metadata, IMoviesRepository moviedata)
+        {
+            _metadata = metadata;
+            _moviedata = moviedata;
+        }
+        public Statsdata[] Get()
+        {
+            try
+            {
+                /// getting  grouped metadata with english title
+                var metadata = _metadata.GetMetadataFilter();
+
+                var lookup = _moviedata.GetStatsLookup();
+
+                return GetStats(metadata, lookup)
+                    .OrderByDescending(x => x.Watches)
+                    .ThenByDescending(x => x.ReleaseYear)
+                    .ToArray();
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //getting metdata value corresponding to each value in watch csv file
+        private static IEnumerable<Statsdata> GetStats(Metadata[] movies, ILookup<int, int> statsLookup)
+        {
+                foreach (var movie in movies)
+                {
+                    var (watches, averageDuration) = GetWatchStats(statsLookup, movie.MovieId);
+                    yield return new Statsdata
+                    {
+                        MovieId = movie.MovieId,
+                        Title = movie.Title,
+                        AverageWatchDurationS = averageDuration,
+                        Watches = watches,
+                        ReleaseYear = movie.ReleaseYear,
+                    };
+                }
+
+
+        }
+
+        //finding avg watch time for given set of movie id
+        private static (int watches, int averageDuration) GetWatchStats(ILookup<int, int> statsLookup, int movieId)
+        {
+            try
+            {
+                var averageDuration = (int)statsLookup[movieId].Average() / 1000;
+                return (statsLookup[movieId].Count(), averageDuration);
+            }
+                        catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
