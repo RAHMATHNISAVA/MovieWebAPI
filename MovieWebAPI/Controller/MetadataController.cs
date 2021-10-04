832abdbcb58efbc07aa8b511180776d94946fc0f
RAHMATHNISAVA <ramathnisa28@gmial.com>
2021-10-04 09:58:51 +0100

    Add files via upload

---
+++ MovieWebAPI/Controller/MetadataController.cs
@@ -0,0 +1,66 @@
+﻿using Boxed.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieWebApplication.Model;
+using MovieWebApplication.Services.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MetadataController : ControllerBase
+    {
+        private readonly ILogger<MetadataController> _logger;
+        private readonly IMetadataService _service;
+
+        public MetadataController(ILogger<MetadataController> logger, IMetadataService service)
+        {
+            _logger = logger;
+            _service = service;
+
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var metadata = _service.Get(id);
+                if (!metadata.Any())
+                    throw new HttpException(404, "File Not Found");
+                return Ok(metadata);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Post(Metadata model)
+        {
+            try
+            {
+                _logger.LogInformation("Started:MetadataController:Post");
+                var result = _service.Post(model);
+                _logger.LogInformation("Finished:MetadataController:Post" + result);
+                if (result)
+                    return Ok();
+                return BadRequest();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error:MetadataController:Post" + ex.ToString());
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ MovieWebAPI/Controller/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieWebApplication.Services.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+        private readonly IMovieService _service;
+
+        public MoviesController(ILogger<MoviesController> logger, IMovieService service)
+        {
+            _logger = logger;
+            _service = service;
+
+        }
+        [HttpGet]
+        [Route("stats")]
+        public IActionResult Get()
+        {
+            try
+            {
+                _logger.LogInformation("Started:MoviesController:Get");
+                var stats = _service.Get();
+                _logger.LogInformation("Finished:MoviesController:Get"+ stats);
+                return Ok(stats);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError("Error:MoviesController:Get" + ex.ToString());
+                throw ex;
+
+            }
+
+        }
+    }
+}
