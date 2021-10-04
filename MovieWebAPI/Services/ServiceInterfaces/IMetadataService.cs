9041bd8714a5a0f1003fae6e49481856f98fd300
RAHMATHNISAVA <ramathnisa28@gmial.com>
2021-10-04 10:10:41 +0100

    Add files via upload

---
+++ MovieWebAPI/Services/ServiceInterfaces/IMetadataService.cs
@@ -0,0 +1,14 @@
+﻿using MovieWebApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Services.ServiceInterface
+{
+   public interface IMetadataService
+    {
+        Metadata[] Get(int movieId);
+        bool Post(Metadata metadata);
+    }
+}
---
+++ MovieWebAPI/Services/ServiceInterfaces/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieWebApplication.Model;
+
+namespace MovieWebApplication.Services.ServiceInterface
+{
+   public interface IMovieService
+   {
+        Statsdata[] Get();
+    }
+}
