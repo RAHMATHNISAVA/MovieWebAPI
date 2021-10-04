e0d3214b5beb4f030f99f3f44acd0db00ddeb07f
RAHMATHNISAVA <ramathnisa28@gmial.com>
2021-10-04 09:59:48 +0100

    Add files via upload

---
+++ MovieWebAPI/Common/MetadataCheck.cs
@@ -0,0 +1,20 @@
+﻿using MovieWebApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Common
+{
+    public static class MetadataCheck
+    {
+        public static bool IsValid(this Metadata m)
+        {
+            return m.Title.HasValue() &&
+                   m.Language.HasValue() &&
+                   m.Duration.HasValue();
+        }
+
+        private static bool HasValue(this string value) => !string.IsNullOrEmpty(value);
+    }
+}
